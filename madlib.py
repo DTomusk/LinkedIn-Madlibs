9667f7c9b43d8c0649f02d1af8594ff15fb25c43
DTomusk <david.tomusk@gmail.com>
2020-07-01 17:53:41 +0100

    Now randomly generates posts but some options in the table are still empty

--- madlib.py
+++ madlib.py
@@ -1,5 +1,6 @@
 import json
 import re
+import random 
 
 # take a template and insert random words from different categories into the tags 
 # print the templates next to numbers and then let the user choose a number which wil randomly generate a post accordingly
@@ -22,17 +23,15 @@ def main():
 	# can use string format to insert value in braces, but what do we do when there's more than one element of the same type?
 	# we could do until re.match doesn't match any values 
 
-	print(template)
+
+	# this ought to be a new function 
 	num_matches = len(re.findall('\{[^, ]+\}', template))
-	print(num_matches)
 
 	for i in range(num_matches):
 		sub_string = re.search('\{[^, ]+\}', template).group(0)
-		print(sub_string)
-		first_element = data[sub_string.strip('{}')][0]
-		print(first_element)
+		var_range = len(data[sub_string.strip('{}')])
+		first_element = data[sub_string.strip('{}')][random.randint(0, var_range-1)]
 		template = re.sub(sub_string, first_element, template, 1)
-		print(template)
 
 	print(template)
 

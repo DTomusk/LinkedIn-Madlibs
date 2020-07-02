4ea3353675bbfc9a332c734bdc960a5cd14d5044
DTomusk <david.tomusk@gmail.com>
2020-07-02 13:39:38 +0100

    Can actually tweet now

--- twitter.py
+++ twitter.py
@@ -4,25 +4,29 @@ from selenium.webdriver.common.keys import Keys # allows selenium to use the key
 
 class LincolnIngram():
 	def __init__(self, username, password):
-		self.browser=webdriver.Chrome("./chromedriver")
+		self.driver = webdriver.Chrome("./chromedriver")
 		self.username = username
 		self.password = password
 
 	def login(self):
-		self.browser.get("https://twitter.com/explore")
+		self.driver.get("https://twitter.com/explore")
+		self.driver.set_window_size(1600, 1200)
 		time.sleep(5) 	# just like a real boy 
-		self.browser.find_element_by_name("session[username_or_email]").send_keys(self.username)
+		self.driver.find_element_by_name("session[username_or_email]").send_keys(self.username)
+		self.driver.find_element_by_name("session[password]").send_keys(self.password)
 		time.sleep(5)
-		self.browser.find_element_by_name("session[password]").send_keys(self.password).send_keys(Keys.ENTER)
+		self.driver.find_element_by_name("session[password]").send_keys(Keys.ENTER)
 
 	def tweet(self, text):
 		time.sleep(5)
-		tweet_box = self.browser.find_element_by_xpath('''//*[@id='react-root']/div/div/div[2]/main/div/div/div/div/div
+		tweet_box = self.driver.find_element_by_xpath('''//*[@id='react-root']/div/div/div[2]/main/div/div/div/div/div
                                                   /div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div
                                                   /div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div
                                                   /div/div/div''')
 		tweet_box.send_keys(text)
+		time.sleep(10)
 		tweet_box.send_keys(Keys.COMMAND, Keys.ENTER)
+		time.sleep(5)
 		
 
 
